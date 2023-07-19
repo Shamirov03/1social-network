f15693d20912f1d0417bf1e2e493f3a4d245eb8d
Your Name <you@example.com>
2023-07-19 21:30:30 +0600

    add

---
+++ Posts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Post, Reaction, Tag, SavedPost
+
+admin.site.register(Post)
+admin.site.register(Reaction)
+admin.site.register(Tag)
+admin.site.register(SavedPost)
+
